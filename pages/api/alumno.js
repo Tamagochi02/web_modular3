c15940f63bc4a38690cf95f8e90e80847f74c98b
Tamagochi02 <mirytamara@gmail.com>
2023-03-01 09:45:47 -0600

    add alumno requests

---
+++ pages/api/alumno.js
@@ -0,0 +1,24 @@
+import { privateRoute } from "../../lib/ironSessionConfig";
+import {get,post,put} from "./../../backend/controllers/alumnoController"
+
+// Funcion CRUD que conecta al controlador de alumno:
+function alumno(req, res) {
+    switch (req.method) {
+        case 'GET':
+            get(req, res)
+            break;
+        case 'POST':
+            post(req, res)
+            break;
+
+        case 'PUT':
+            put(req, res)
+            break;
+
+        default:
+            res.status(400).json({})
+            break;
+    }
+}
+
+export default privateRoute(alumno) // funcion para asignar ruta privada enviando un handler, en este caso alumno
