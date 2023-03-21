aa321748fcc80348e07f632982d1b83e80034807
Tamagochi02 <mirytamara@gmail.com>
2023-03-21 15:28:54 -0600

    add proyecto requests

---
+++ pages/api/proyecto/index.js
@@ -0,0 +1,22 @@
+import {get,post,put} from "../../../backend/controllers/proyectoController"
+
+// Funcion CRUD que conecta al controlador del proyecto:
+export default function proyecto(req, res) {
+    switch (req.method) {
+        case 'GET':
+            get(req, res)
+            break;
+
+        case 'POST':
+            post(req, res)
+            break;
+            
+        case 'PUT':
+            put(req, res)
+            break;
+
+        default:
+            res.status(400).json({})
+            break;
+    }
+}
