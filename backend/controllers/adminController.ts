c3310c65769e0799a279b01dbaa70241801d7cb1
JazOki <castellanossilvia537@gmail.com>
2023-04-18 21:01:45 -0600

    BE: Add admin

---
+++ backend/controllers/adminController.ts
@@ -0,0 +1,39 @@
+import { Rol } from '@prisma/client'
+import type { NextApiHandler } from 'next'
+import { exclude, prisma } from "../../lib/prisma"
+
+export const readAdmin: NextApiHandler = async (_req, res) => {
+    const users = await prisma.usuario.findMany({
+        where: { rol: Rol.Administrador }
+    })
+    res.json(users)
+}
+
+export const createAdmin: NextApiHandler = async (req, res) => {
+    const {
+        nombre, correo, contrasena
+    } = req.body
+    const user = await prisma.usuario.create({
+        data: {
+            rol: Rol.Administrador,
+            nombre, correo, contrasena
+        }
+    })
+    res.json(user)
+}
+
+export const updateAdmin: NextApiHandler = async (req, res) => {
+    const {
+        nombre, correo, contrasena,
+        foto, estaActivo, id, adminId
+    } = req.body
+    const user = await prisma.usuario.update({
+        data: {
+            nombre, correo, contrasena, foto, estaActivo
+        },
+        where: {
+            id
+        }
+    })
+    res.json(exclude(user, ['contrasena']))
+}
