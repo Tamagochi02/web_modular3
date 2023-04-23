297d6ff2790141340ad443d86389832eeb972f4f
Tamagochi02 <mirytamara@gmail.com>
2023-04-23 01:30:40 -0600

    feat(backend): add readProyectById query

---
+++ pages/api/obtenerProyectoPorId.ts
@@ -0,0 +1,17 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import type { IronNextApiHandler } from '../../lib/ironSessionConfig'
+import { ironOptions } from '../../lib/ironSessionConfig'
+import { prisma, exclude } from "../../lib/prisma";
+
+const readProyectById: IronNextApiHandler = async (req, res) => {
+    const {id} = req.body
+    const proyecto = await prisma.proyecto.findFirst({
+        where: {
+            id: id
+            }
+        }
+    )
+    res.json(proyecto);
+}
+
+export default withIronSessionApiRoute(readProyectById, ironOptions)
