335f7fe680bad4857b8f87a85929622a86e6c60b
JazOki <castellanossilvia537@gmail.com>
2023-05-09 04:55:34 -0600

    FE: Viewer in pdf format

--- pages/pdf/[id]/estado_de_arte.jsx
+++ pages/pdf/[id]/estado_de_arte.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from 'next/router'
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
+import Card from "../../../components/Card";
 
 const handleDownload = async () => {
   const pdf = new jsPDF();
@@ -11,7 +12,7 @@ const handleDownload = async () => {
   const canvas = await html2canvas(container);
   const imgData = canvas.toDataURL("image/png");
 
-  const imgWidth = 210; 
+  const imgWidth = 210;
   const pageHeight = 297;
   const imgHeight = (canvas.height * imgWidth) / canvas.width;
   let heightLeft = imgHeight;
@@ -28,14 +29,13 @@ const handleDownload = async () => {
     heightLeft -= pageHeight;
   }
 
-  pdf.save("archivo.pdf");
+  pdf.save("Estado_del_arte.pdf");
 };
 
 const PDFContent = () => {
   const router = useRouter()
   const { id } = router.query
   const [documents, setDocuments] = useState([])
-  const [documentos, setDocumentos] = useState([]);
 
 
   useEffect(() => {
@@ -46,7 +46,7 @@ const PDFContent = () => {
       console.log(res);
 
     }).catch((error) => {
-      toast("Error al obtener los datos");
+      toast.error("Error al obtener los datos");
       console.log(error)
     });
   }, [id])
@@ -55,83 +55,106 @@ const PDFContent = () => {
   if (!documents)
     return <p>Loading...</p>
   console.log(documents);
+  console.log(documents.DocEtapa1);
 
 
 
   // código para obtener los datos y mostrar el contenido del PDF
 
   return (
-    <div id="pdf-container" className="felx flex-col container">
-      {/* Tu código para mostrar el contenido del PDF */}
+    <Card>
+      <button onClick={handleDownload} class="bg-blue-500 hover:bg-blue-400 text-gray-darkest font-bold py-2 px-4 rounded inline-flex items-center">
+        <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+        <span>Descargar</span>
+      </button>
+      
+      <div id="pdf-container" className="felx flex-col container ">
+        {/* Tu código para mostrar el contenido del PDF */}
 
-      <div className='flex justify-center'>
-        <img src="/logo.png" alt="" className=" mt-20 w-full lg:w-1/3 xl:w-1/5" />
-      </div>
+        <div className='flex justify-center'>
+          <img src="/logo.png" alt="" className=" mt-20 w-full lg:w-1/3 xl:w-1/5" />
+        </div>
 
-      <div className=' mt-4 text-center font-bold text-lg'>
-        <h1 className=''>CENTRO UNIVERSITARIO UNE</h1>
-        <h1 className=''>INGENIERÍA EN COMPUTACIÓN</h1>
-        <h1 className=''>PROYECTO MODULAR</h1>
-      </div>
+        <div className=' mt-4 text-center font-bold text-lg'>
+          <h1 className=''>CENTRO UNIVERSITARIO UNE</h1>
+          <h1 className=''>INGENIERÍA EN COMPUTACIÓN</h1>
+          <h1 className=''>PROYECTO MODULAR</h1>
+        </div>
 
-      <p className='text-center'>
-      </p>
-      <p className='text-center'>{documents.nombre}</p>
-      <h1 className=' mt-4 text-center font-bold text-lg'>ESTADO DEL ARTE</h1>
-      <p className='text-center'>{documents.titulo}</p>
-      {/* <h1 className=' mt-4 text-center font-bold text-lg'>INTEGRANTES:</h1>
+        <p className='text-center'>
+        </p>
+        <p className='text-center'>{documents.nombre}</p>
+        <h1 className=' mt-4 text-center font-bold text-lg'>ESTADO DEL ARTE</h1>
+        <p className='text-center'>{documents.titulo}</p>
+        {/* <h1 className=' mt-4 text-center font-bold text-lg'>INTEGRANTES:</h1>
       <p className='text-center'>Obtener los integrantes</p> */}
 
-      <h1 className=' mt-4 text-center font-bold text-lg'>INDICE</h1>
-      <h1 className=' ml-60 mt-4 font-bold text-lg text-blue-500'>Contenido</h1>
-      <div className=' ml-60 mt-4 font-bold text-lg '>
+        <h1 className=' mt-4 text-center font-bold text-lg'>INDICE</h1>
+        <br />
+        <h1 className=' ml-60 mt-4 font-bold text-lg text-blue-500'>Contenido</h1>
+        <div className=' ml-60 mt-4 font-bold text-lg '>
 
-        <h1 className=''>RESUMEN</h1>
-        <h1 className=''>PALABRAS CLAVE</h1>
-        <h1 className=''>INTRODUCCIÓN</h1>
-        <h1 className=''>DESARROLLO DEL TEMA</h1>
-        <h1 className=''>CONCLUSIONES</h1>
-        <h1 className=''>REFERENCIAS</h1>
+          <h1 className=''>RESUMEN</h1>
+          <h1 className=''>PALABRAS CLAVE</h1>
+          <h1 className=''>INTRODUCCIÓN</h1>
+          <h1 className=''>DESARROLLO DEL TEMA</h1>
+          <h1 className=''>CONCLUSIONES</h1>
+          <h1 className=''>REFERENCIAS</h1>
 
+          <br />
+
+        </div>
+
+        {/* <div className="border-t"></div> */}
+
+        <div className='ml-36 mt-32 text-lg text-justify max-w-full mx-32'>
+
+          <h1 className='font-bold'>RESUMEN</h1>
+          <p className=' '>
+            {typeof documents !== 'undefined' ?
+              typeof documents.DocEtapa1 !== 'undefined' ?
+                documents.DocEtapa1.length > 0 ? documents.DocEtapa1[0]?.resumen : '' : '' : ''}
+          </p>
+
+          <br />
+          <h1 className='font-bold'>PALABRAS CLAVE</h1>
+          <p className=''>{typeof documents !== 'undefined' ?
+            typeof documents.DocEtapa1 !== 'undefined' ?
+              documents.DocEtapa1.length > 0 ? documents.DocEtapa1[0]?.palabrasClave : '' : '' : ''}</p>
+          <br />
+
+          <h1 className='font-bold'>INTRODUCCIÓN</h1>
+          <p className=''>{typeof documents !== 'undefined' ?
+            typeof documents.DocEtapa1 !== 'undefined' ?
+              documents.DocEtapa1.length > 0 ? documents.DocEtapa1[0]?.introduccion : '' : '' : ''}</p>
+          <br />
+          <h1 className='font-bold'>DESARROLLO DEL TEMA</h1>
+          <p className=''>{typeof documents !== 'undefined' ?
+            typeof documents.DocEtapa1 !== 'undefined' ?
+              documents.DocEtapa1.length > 0 ? documents.DocEtapa1[0]?.desarrollo : '' : '' : ''}</p>
+          <br />
+          <h1 className='font-bold'>CONCLUSIONES</h1>
+          <p className=''>{typeof documents !== 'undefined' ?
+            typeof documents.DocEtapa1 !== 'undefined' ?
+              documents.DocEtapa1.length > 0 ? documents.DocEtapa1[0]?.conclusion : '' : '' : ''}</p>
+          <br />
+          <h1 className='font-bold'>REFERENCIAS</h1>
+          <p className=''>{typeof documents !== 'undefined' ?
+            typeof documents.DocEtapa1 !== 'undefined' ?
+              documents.DocEtapa1.length > 0 ? documents.DocEtapa1[0]?.referencias : '' : '' : ''}</p>
+        </div>
         <br />
 
+
+        {/* 
+        <button onClick={handleDownload}>Descargar PDF</button> */}
+
+
+
       </div>
 
 
-      <div className='ml-45 mt-4 text-lg text-justify'>
-        <h1 className='font-bold'>RESUMEN</h1>
-        {/* <p className=''>{documents.DocEtapa1[0]?.resumen}</p> */}
-        <h1 className='font-bold'>PALABRAS CLAVE</h1>
-        {/* <p className=''>{documents.DocEtapa1[0]?.palabrasClave}</p> */}
-        <h1 className='font-bold'>INTRODUCCIÓN</h1>
-        {/* <p className=''>{documents.DocEtapa1[0]?.introduccion}</p> */}
-        <h1 className='font-bold'>DESARROLLO DEL TEMA</h1>
-        {/* <p className=''>{documents.DocEtapa1[0]?.desarrollo}</p> */}
-        <h1 className='font-bold'>CONCLUSIONES</h1>
-        {/* <p className=''>{documents.DocEtapa1[0]?.conclusion}</p> */}
-        <h1 className='font-bold'>REFERENCIAS</h1>
-        {/* <p className=''>{documents.DocEtapa1[0]?.referencias}</p> */}
-      </div>
-
-
-
-      <button onClick={handleDownload}>Descargar PDF</button>
-
-
-      {/* <div dir="rtl">
-        <div className="relative h-32 w-32">
-          <div class="w-64 flex flex-col px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-purple-700 hover:text-white">
-            <button onClick={handleDownload} class="mt-2 text-base leading-normal"
-
-            >
-              Descargar PDF
-
-            </button>
-
-          </div>
-        </div>
-      </div> */}
-    </div>
+    </Card>
 
 
   );
--- pages/pdf/[id]/resumen.jsx
+++ pages/pdf/[id]/resumen.jsx
@@ -1,72 +1,148 @@
 import React from 'react';
 import { useRouter } from 'next/router'
 import { useEffect, useState } from "react";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import Card from "../../../components/Card";
 
+const handleDownload = async () => {
+  const pdf = new jsPDF();
+  const container = document.getElementById("pdf-container");
+
+  const canvas = await html2canvas(container);
+  const imgData = canvas.toDataURL("image/png");
+
+  const imgWidth = 210;
+  const pageHeight = 297;
+  const imgHeight = (canvas.height * imgWidth) / canvas.width;
+  let heightLeft = imgHeight;
+
+  let position = 0;
+
+  pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+  heightLeft -= pageHeight;
+
+  while (heightLeft >= 0) {
+    position = heightLeft - imgHeight;
+    pdf.addPage();
+    pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+    heightLeft -= pageHeight;
+  }
+
+  pdf.save("Resumen.pdf");
+};
 
 
 const PDFContent = () => {
   const router = useRouter()
-  const { docid: documentId, id: proyectId } = router.query
+  const { id } = router.query
   const [documents, setDocuments] = useState()
   const [proyects, setProyects] = useState()
 
   useEffect(() => {
     Promise.all([
-      fetch(`/api/projects/${proyectId}`).then((response) => response.json()),
-      fetch(`/api/documents/${documentId}`).then((response) => response.json())
-    ]).then(([pro, res]) => {
+      fetch(`/api/documents/${id}`).then((response) => response.json())
+    ]).then(([res]) => {
       setDocuments(res)
-      setProyects(pro)
     }).catch((error) => {
       toast("Error al obtener los datos");
       console.log(error)
     });
-  }, [])
+  }, [id])
+
+  if (!documents)
+    return <p>Loading...</p>
+  console.log(documents);
+  console.log(documents.DocEtapa2);
+
+  // código para obtener los datos y mostrar el contenido del PDF
 
   return (
-    <div className='felx flex-col container'>
-      <div className='flex justify-center'>
-        <img src="encabezado.jpeg" alt="" />
+    <Card>
+      <button onClick={handleDownload} class="bg-blue-500 hover:bg-blue-400 text-gray-darkest font-bold py-2 px-4 rounded inline-flex items-center">
+        <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+        <span>Descargar</span>
+      </button>
+
+      <div id="pdf-container" className='felx flex-col container '>
+        <div className='max-w-full mx-32'>
+          <div className='flex justify-center'>
+            <img src="/encabezado.jpeg" alt="" />
+          </div>
+
+          <div>
+            <h1 className=' mt-4 text-center font-bold text-lg'>PROYECTO DEL MÓDULO</h1>
+            <div className='text-center' name='nombre modulo'>
+              <p className='text-center'>{documents.nombre}</p>
+            </div>
+            <h1 className=' mt-4 text-center font-bold text-lg'>TÍTULO</h1>
+            <p className='text-center'>{documents.titulo}</p>
+            <h1 className=' mt-4 text-center font-bold text-lg'>Descripción</h1>
+            <p className=' text-center justify-center ml-56 text-lg max-w-full mx-32'>
+              {typeof documents !== 'undefined' ?
+                typeof documents.DocEtapa2 !== 'undefined' ?
+                  documents.DocEtapa2.length > 0 ? documents.DocEtapa2[0]?.descripcion : '' : '' : ''}
+            </p>
+            {/* <h1 className=' mt-4 text-center font-bold text-lg'>Integrantes del proyecto:</h1> */}
+            {/* <div className='flex justify-center mt-4'>
+          <table>
+            <thead>
+              <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+                <th className="bg-gray-400 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Nombre</th>
+                <th className="bg-gray-400 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Código</th>
+                <th className="bg-gray-400 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Matrícula</th>
+                <th className="bg-gray-400 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Correo electrónico</th>
+
+              </tr>
+
+            </thead>
+            <tbody>
+              //{proyects && proyects.usuarios && proyects.usuarios.map((up, index) => ( 
+              <tr>
+                <td className="border px-4 py-2"></td>
+                <td className="border px-4 py-2"></td>
+                <td className="border px-4 py-2"></td>
+                <td className="border px-4 py-2"></td>
+              </tr>
+            </tbody>
+          </table>
+          </div> */}
+          </div>
+
+        </div>
+
+        <div className='ml-56 mt-32 text-lg text-justify max-w-full mx-32'>
+          <h1 className='font-bold '>Objetivo general</h1>
+          <p className='indent-16'>
+            {typeof documents !== 'undefined' ?
+              typeof documents.DocEtapa2 !== 'undefined' ?
+                documents.DocEtapa2.length > 0 ? documents.DocEtapa2[0]?.objGeneral : '' : '' : ''}
+          </p>
+          <br />
+          <h1 className='font-bold'>Objetivos y metas específicas</h1>
+          <p className='indent-16 '>
+            {typeof documents !== 'undefined' ?
+              typeof documents.DocEtapa2 !== 'undefined' ?
+                documents.DocEtapa2.length > 0 ? documents.DocEtapa2[0]?.objsMetas : '' : '' : ''}
+          </p>
+          <br />
+          <h1 className='font-bold'>Alcance</h1>
+          <p className='indent-16 '>
+            {typeof documents !== 'undefined' ?
+              typeof documents.DocEtapa2 !== 'undefined' ?
+                documents.DocEtapa2.length > 0 ? documents.DocEtapa2[0]?.alcance : '' : '' : ''}
+          </p>
+          <br />
+          <h1 className='font-bold'>Herramientas</h1>
+          <p className='indent-16'>
+            {typeof documents !== 'undefined' ?
+              typeof documents.DocEtapa2 !== 'undefined' ?
+                documents.DocEtapa2.length > 0 ? documents.DocEtapa2[0]?.herramientas : '' : '' : ''}
+          </p>
+        </div>
+        <br />
       </div>
-
-      <h1 className=' mt-4 text-center font-bold text-lg'>PROYECTO DEL MÓDULO</h1>
-      <div className='text-center' name='nombre modulo'>
-        <p></p>
-      </div>
-      <h1 className=' mt-4 text-center font-bold text-lg'>TÍTULO</h1>
-      <h1 className=' mt-4 text-center font-bold text-lg'>Descripción</h1>
-      <h1 className=' mt-4 text-center font-bold text-lg'>Integrantes del proyecto:</h1>
-      <div className='flex justify-center mt-4'>
-        <table>
-          <thead>
-            <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-              <th className="bg-gray-400 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Nombre</th>
-              <th className="bg-gray-400 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Código</th>
-              <th className="bg-gray-400 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Matrícula</th>
-              <th className="bg-gray-400 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Correo electrónico</th>
-
-            </tr>
-
-          </thead>
-          <tbody>
-            {/* {proyects && proyects.usuarios && proyects.usuarios.map((up, index) => ( */}
-            <tr>
-              <td className="border px-4 py-2"></td>
-              <td className="border px-4 py-2"></td>
-              <td className="border px-4 py-2"></td>
-              <td className="border px-4 py-2"></td>
-            </tr>
-            {/* ))} */}
-          </tbody>
-        </table>
-      </div>
-
-
-      <h1 className=' ml-60 mt-4 font-bold text-lg '>Objetivo general</h1>
-      <h1 className=' ml-60 mt-4 font-bold text-lg'>Objetivos y metas específicas</h1>
-      <h1 className=' ml-60 mt-4 font-bold text-lg'>Alcance</h1>
-      <h1 className=' ml-60 mt-4 font-bold text-lg'>Herramientas</h1>
-    </div>
+    </Card>
   );
 }
 
